d7458510b2412fb4ee2df07b383bd22aab70c527
IfyN <31158530+IfyN@users.noreply.github.com>
2024-03-01 12:11:54 +0100

    Update cursor value

--- scripts.js
+++ scripts.js
@@ -24,6 +24,7 @@ const fetchMoreAuthors = () => {
   displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
   if (authorDataArr.length <= endingIndex) {
     loadMoreBtn.disabled = true;
+    loadMoreBtn.style.cursor = 'not-allowed';
     loadMoreBtn.textContent = 'No more data to load';
   }
 };
@@ -34,10 +35,10 @@ const displayAuthors = (authors) => {
     authorContainer.innerHTML += `
     <div id="${index}" class="user-card">
       <h2 class="author-name">${author}</h2>
-      <img class="user-img" src="${image}" alt="${author} avatar" />
-       <div class="purple-divider"></div>
+      <img class="user-img" src="${image}" alt="${author} avatar">
+      <div class="purple-divider"></div>
       <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + '...' : bio}</p>
-      <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
+      <a class="author-link" href="${url}" target="_blank">${author} author page</a>
     </div>
   `;
   });
